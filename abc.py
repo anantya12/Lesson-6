44c93e0c7a3fb7e47e77d5952fab3998ac2e0e2d
anantya12 <verma.anantya12@gmail.com>
2026-02-14 12:40:19 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,24 @@
+a = int(input("Please enter a nuber for a: "))
+b = int(input("Please enter a nuber for b: "))
+c = int(input("Please enter a nuber for c: "))
+
+if a and b and c:
+    print("All the numbers have boolean value as True")
+else:
+    print("Atleast one number has boolean value as False")
+
+
+x = int(input("Please enter a nuber for x: "))
+y = int(input("Please enter a nuber for y: "))
+z = int(input("Please enter a nuber for z: "))
+
+if x > 0 or y > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+
+
+if y > 0 or z > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
